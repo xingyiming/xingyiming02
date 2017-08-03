48408f65e824c90fa12415efa7714030f83bd76e
‘xingyiming’ <260892184@qq.com>
2017-08-03 23:49:46 +0800

    Initial commit

---
+++ src/main/java/xingyiming02.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/8/3.
+ */
+public class xingyiming02 {
+    @Test
+    public void xingyiming02() {
+        System.out.println("hello word");
+    }
+}
